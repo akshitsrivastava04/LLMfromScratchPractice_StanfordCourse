f8462127a27d1b35f37fdc8feb64774582b1800b
Akshit Srivastava <88588444+restlesshornet@users.noreply.github.com>
2025-12-03 01:09:33 +0530

    small update

--- Attention/SelfAttention.py
+++ Attention/SelfAttention.py
@@ -1,22 +1,35 @@
 import torch.nn as nn 
 import torch
 import torch.nn.functional as F
+import math
 
 
 class Linear(nn.Module):
     def __init__(self, d_in, d_out, bias=True):
         super().__init__()
-        self.W = torch.randn(d_in, d_out) * (d_in ** -0.5)
-        self.W.requires_grad = True 
+        
+        W = torch.empty(d_in, d_out) 
+        W = nn.Parameter(W)   
+        self.W = W
 
         if bias: 
-            self.b = torch.zeros(d_out)
-            self.b.requires_grad = True 
+            b_tensor = torch.empty(d_out, requires_grad=True)
+            self.register_parameter("b", nn.Parameter(b_tensor))
         else: 
             self.b = None 
+        
+        self.reset_parameters()
+
+    def reset_parameters(self): 
+        fan_in = self.W.size(0)
+        bound = 1 / math.sqrt(fan_in)
+        torch.nn.init.kaiming_uniform_(self.W, a=math.sqrt(5))
+        if self.b is not None: 
+            torch.nn.init.uniform_(self.b, -bound, bound)
 
     def forward(self, x): 
-        y = torch.einsum("bsi, io -> bso", x, self.W)
+        y = torch.einsum("bsi,io->bso", x, self.W)
+
         if self.b is not None: 
             y = y + self.b
         return y 

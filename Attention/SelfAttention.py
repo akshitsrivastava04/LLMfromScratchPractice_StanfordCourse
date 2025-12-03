37d84ae61e92f92d3a79b0ed32ade408e819a9bb
Akshit Srivastava <88588444+restlesshornet@users.noreply.github.com>
2025-12-03 18:22:58 +0530

    casualattention

--- Attention/SelfAttention.py
+++ Attention/SelfAttention.py
@@ -34,6 +34,27 @@ class Linear(nn.Module):
             y = y + self.b
         return y 
 
+class Dropout(nn.Module):
+    def __init__(self,p=0.5, inplace=False):
+        super(Dropout, self).__init__()
+        if p<0 or p>1:
+            raise ValueError("Dropout probability must be between 0 and 1 but got {}".format(p))
+        self.p = p 
+        self.inplace = inplace 
+
+    def forward(self, input):
+        if self.training:
+            mask = (torch.rand_like(input) > self.p).float()
+            return mask * input / (1 - self.p)
+        return input
+
+    def __repr__(self):
+        inplace_str = ', inplace' if self.inplace else ''
+        return self.__class__.__name__ + '(' \
+            + 'p=' + str(self.p) \
+            + inplace_str + ')'
+
+
 class SelfAttention_v1(nn.Module): 
     def __init__(self, d_in, d_out): 
         super().__init__()
@@ -75,6 +96,31 @@ class SelfAttention_v2(nn.Module):
         context_vec = torch.einsum('bqk,bkv->bqv', attn_weights, values)
         return context_vec
 
+class CasualAttention(nn.Module): 
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out 
+        self.W_query = Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = Dropout(dropout)
+        mask = torch.triu(torch.ones(context_length, context_length), diagonal=1).to(torch.bool)
+        self.register_buffer("mask", mask)
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape 
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = torch.einsum("bqd, bkd -> bqk", queries, keys)
+        attn_scores.masked_fill_(self.mask[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+        self.attn_weights = attn_weights
+        context_vec = torch.einsum('bqk,bkv->bqv', attn_weights, values)
+        return context_vec
+        
 if __name__ == "__main__":
     torch.manual_seed(123)
 
@@ -101,3 +147,36 @@ if __name__ == "__main__":
     loss.backward()
 
     print(f"\nGradient calculated for W_query: {sa_v2.W_query.W.grad is not None}")
+
+    queries = sa_v2.W_query(inputs)
+    keys = sa_v2.W_key(inputs)
+    values = sa_v2.W_value(inputs)
+    
+    print("\nQueries shape:", queries.shape)
+    print("Keys shape:", keys.shape)
+    print("Values shape:", values.shape)
+
+    attn_scores = torch.einsum("bqd, bkd -> bqk", queries, keys)
+    print("\nAttention scores shape:", attn_scores.shape)
+    attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+    print("\nAttention weights shape:", attn_weights.shape)
+    context_vec = torch.einsum('bqk,bkv->bqv', attn_weights, values)
+    print("\nContext vector shape:", context_vec.shape)
+    context_lenght = attn_scores.shape[0]
+    mask_simple = torch.tril(torch.ones(context_lenght, context_lenght)) 
+    print(mask_simple)
+
+    print("\n--- Testing CasualAttention ---")
+    context_len = 5
+    ca = CasualAttention(d_in, d_out, context_length=context_len, dropout=0.1, qkv_bias=True)
+
+    x = torch.randn(2, 5, d_in)   # (batch, seq_len, dim)
+    y2 = ca(x)
+
+    print("CasualAttention Output shape:", y2.shape)
+
+    loss2 = y2.sum()
+    loss2.backward()
+
+    print("Grad W_query:", ca.W_query.W.grad is not None)
+    print("Mask used:\n", ca.mask[:5, :5])
